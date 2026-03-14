e488ac371c83fd7fb23b193127ab37d54fb09680
Rizkya Dwi Aulya Fasya <119921159+LyaFasya@users.noreply.github.com>
2026-03-14 12:44:28 +0700

    Update Simpanin

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express")
-const app = express();
-const PORT = process.env.PORT || 8000;
+const app = express()
+const PORT = process.env.PORT || 8000
 const authRoutes = require('./routes/auth.route')
 
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/auth', authRoutes)
 
-module.exports = app;
+module.exports = app
 
 require("dotenv").config()
 
